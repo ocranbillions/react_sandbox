4f2c41bec40e9a37f33a6014c3135feb74f18f76
Samuel Ocran <sammiestt@gmail.com>
2020-03-07 16:53:51 +0100

    Comment-out second logic

--- src/components/FormValidator/FormValidator.js
+++ src/components/FormValidator/FormValidator.js
@@ -4,19 +4,22 @@ import './styles.css'
 class FormValidator extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            username: '',
-            email: '',
-            password: '',
-            password2: '',
-
-            incorrectUsername: null,
-            incorrectEmail: null,
-            incorrectPassword: null,
-            incorrectPassword2: null,
-        };
+        this.state = this.initialState;
     }
-    
+    initialState = {
+        username: '',
+        email: '',
+        password: '',
+        password2: '',
+
+        usernameError: null,
+        emailError: null,
+        passwordError: null,
+        password2Error: null,
+
+        // disableSubmit: true,
+    };
+
     handleInputChange = (event) => {
         const field = event.target.id;
         const value = event.target.value;
@@ -30,51 +33,71 @@ class FormValidator extends Component {
                 const validUsername = (value.length >= 3);
                 this.setState({
                     username: value,
-                    incorrectUsername: validUsername ? '' : 'Username must be at least 3',
+                    usernameError: validUsername ? '' : 'Username must be at least 3',
                 })
                 break;
             case 'email':
                 const emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
                 this.setState({
                     email: value,
-                    incorrectEmail: emailValid ? '' : 'Email is invalid',
+                    emailError: emailValid ? '' : 'Email is invalid',
                 })
                 break;
             case 'password':
                 const validPassword = (value.length >= 6);
                 this.setState({
                     password: value,
-                    incorrectPassword: validPassword ? '' : 'Password must be at least 6 characters',
+                    passwordError: validPassword ? '' : 'Password must be at least 6 characters',
                 })
                 break;
             case 'password2':
                 const validPassword2 = (value === this.state.password);
                 this.setState({
                     password2: value,
-                    incorrectPassword2: validPassword2 ? '' : 'Passwords do not match',
+                    password2Error: validPassword2 ? '' : 'Passwords do not match',
                 })
                 break;
           default:
             break;
         }
+
+        // const { 
+        //     usernameError,
+        //     emailError, 
+        //     passwordError, 
+        //     password2Error } = this.state;
+
+        // if(usernameError === "" && 
+        //     emailError === "" && 
+        //     passwordError === "" && 
+        //     password2Error === "")
+        // {
+        //     console.log('workings....')
+        //     this.setState({
+        //         disableSubmit: false,
+        //     })
+
+        // }
         
     }
 
     handleFormSubmission = (event) => {
         event.preventDefault();
-        console.log(this.state)
         const { 
-            incorrectUsername,
-            incorrectEmail, 
-            incorrectPassword, 
-            incorrectPassword2 } = this.state;
+            usernameError,
+            emailError, 
+            passwordError, 
+            password2Error } = this.state;
 
-        if(incorrectUsername === "" && 
-            incorrectEmail === "" && 
-            incorrectPassword === "" && 
-            incorrectPassword2 === "")
+        if(usernameError === "" && 
+            emailError === "" && 
+            passwordError === "" && 
+            password2Error === "")
         {
+            // clear fields
+            this.setState(this.initialState)
             alert('success')
+
         } else {
             alert('incorrect form fields')
         }
@@ -85,32 +108,32 @@ class FormValidator extends Component {
         const {
             username, email,
             password, password2,
-            incorrectUsername, 
-            incorrectEmail,
-            incorrectPassword, 
-            incorrectPassword2 } = this.state;
+            usernameError, 
+            emailError,
+            passwordError, 
+            password2Error } = this.state;
 
 
         return (
             <div className="container">
                 <form id="form" className="form" onSubmit={this.handleFormSubmission}>
                     <h2>Register With Us</h2>
-                    <div className={`form-control${incorrectUsername ? ' error' : ''}`}>
+                    <div className={`form-control${usernameError ? ' error' : ''}`}>
                         <label htmlFor="username">Username</label>
                         <input type="text" id="username" placeholder="Enter username" value={username} onChange={this.handleInputChange}/>
-                        <small>{incorrectUsername}</small>
+                        <small>{usernameError}</small>
                     </div>
-                    <div className={`form-control${incorrectEmail ? ' error' : ''}`}>
+                    <div className={`form-control${emailError ? ' error' : ''}`}>
                         <label htmlFor="email">Email</label>
                         <input type="text" id="email" placeholder="Enter email" value={email} onChange={this.handleInputChange}/>
-                        <small>{incorrectEmail}</small>
+                        <small>{emailError}</small>
                     </div>
-                    <div className={`form-control${incorrectPassword ? ' error' : ''}`}>
+                    <div className={`form-control${passwordError ? ' error' : ''}`}>
                         <label htmlFor="password">Password</label>
                         <input type="password" id="password" placeholder="Enter password" value={password} onChange={this.handleInputChange}/>
-                        <small>{incorrectPassword}</small>
+                        <small>{passwordError}</small>
                     </div>
-                    <div className={`form-control${incorrectPassword2 ? ' error' : ''}`}>
+                    <div className={`form-control${password2Error ? ' error' : ''}`}>
                         <label htmlFor="password2">Confirm Password</label>
                         <input
                             type="password"
@@ -119,9 +142,10 @@ class FormValidator extends Component {
                             value={password2}
                             onChange={this.handleInputChange}
                         />
-                        <small>{incorrectPassword2}</small>
+                        <small>{password2Error}</small>
                     </div>
                     <button type="submit">Submit</button>
+                    {/* <button type="submit" disabled={this.state.disableSubmit}>Submit</button> */}
                 </form>
             </div>
         )
