08b9b7023045b341b60f0312aec36c2e6aad8eb4
Samuel Ocran <sammiestt@gmail.com>
2020-03-13 23:47:27 +0100

    Sidebar & Modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
-// import FormValidator from './components/FormValidator/FormValidator';
-import MovieSeatBooker from './components/MovieSeatBooker/MovieSeatBooker';
+import FormValidator from './components/FormValidator/FormValidator';
+// import MovieSeatBooker from './components/MovieSeatBooker/MovieSeatBooker';
 
 
 class App extends Component {
@@ -9,8 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <MovieSeatBooker/>
-          {/* <FormValidator/> */}
+          {/* <MovieSeatBooker/> */}
+          <FormValidator/>
       </div>
     );
 
