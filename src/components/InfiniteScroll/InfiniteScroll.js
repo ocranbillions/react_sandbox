ade2a861c3a75f736a2f8f8029cc82c9e69bf394
Samuel Ocran <sammiestt@gmail.com>
2020-03-21 21:53:41 +0100

    Infinite Scroll

---
+++ src/components/InfiniteScroll/InfiniteScroll.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class LyricsSearch extends Component {
+    render() {
+        return (
+            <div>
+                Welcome
+            </div>
+        )
+    }
+}
+
+export default LyricsSearch;
